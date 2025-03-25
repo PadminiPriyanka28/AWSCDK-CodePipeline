dc9dc96bc6091b995cf36ce49599bdcba2904fbb
PadminiPriyanka28 <prateekshaporeddy@gmail.com>
2025-03-25 17:43:46 -0400

    Initial Commit

--- lib/assignment2-stack.ts
+++ lib/assignment2-stack.ts
@@ -1,16 +1,43 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 
 export class Assignment2Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Step 5: Create S3 Bucket
+    const myBucket = new s3.Bucket(this, 'MyFirstBucket', {
+      versioned: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // Use RETAIN for production
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'Assignment2Queue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Step 6: Create Lambda Function
+    const myLambda = new lambda.Function(this, 'MyLambda', {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromInline(`
+        exports.handler = async function(event) {
+          console.log('Lambda invoked!');
+          return { statusCode: 200, body: 'Hello, World!' };
+        }
+      `),
+      environment: {
+        BUCKET_NAME: myBucket.bucketName,
+      },
+    });
+
+    // Step 7: Create DynamoDB Table
+    const myTable = new dynamodb.Table(this, 'MyTable', {
+      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+      tableName: 'MyTable',
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    // Grant Lambda permissions to access S3 and DynamoDB
+    myBucket.grantReadWrite(myLambda);
+    myTable.grantReadWriteData(myLambda);
   }
 }
